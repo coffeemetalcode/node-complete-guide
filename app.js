e1172283a28bdc546269188580cd04ccddc8bf36
dbstocker <dstocker@notesettersinc.com>
2019-12-02 21:48:22 -0500

    routes, path, 404 page

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.use('/users', (req, res, next) => {
-  res.send(`
-    <h1>Users Page</h1>
-  `);
-});
-
-app.use('/', (req, res, next) => {
-  res.send(`
-    <h1>Hello from Express.js!</h1>
-  `);
-});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
