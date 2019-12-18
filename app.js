8991d1da805010f2e40af7cfe37799bab77a961e
dbstocker <dstocker@notesettersinc.com>
2019-12-18 14:29:11 -0500

    ejs intro complete

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout'
-}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -26,7 +21,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404)
-    .render('404', {pageTitle: 'Page Not Found'});
+    .render('404', { pageTitle: 'Page Not Found' });
     // .sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
